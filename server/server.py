ce092efa1a5ca85f7ef48a17bb2d336707fb0f2c
mcaupybugs <vishalydv.me@gmail.com>
2024-02-29 01:12:12 +0530

    Adding updated logic

--- server/server.py
+++ server/server.py
@@ -7,25 +7,52 @@ DEFAULT_PORT = 8000
 class SocketServer:
     """SocketServer class to initiate the creation of a socket server and the related options."""
 
-    def __init__(self):
-        self.start_application()
+    def __init__(self, host=DEFAULT_HOST, port=DEFAULT_PORT):
+        try:
+            # TODO : Make this method better
+            if self.__update_connection_ports(host, port):
+                self.host = host
+                self.port = host
+            else :
+                self.host = DEFAULT_HOST
+                self.port = DEFAULT_PORT
 
-    def start_application(self):
+            print(f"Using HOST:PORT {host}:{port}")
+            self.start_server()
+
+        except Exception as e:
+            print("Initialization failed due to exception : " ,e)
+
+    def start_server(self):
         """Entry point of the socket server"""
         try:
-            host = input("Enter the host address to be exposed for connection: ")
-            port = input("Enter the port to be used for the connection to be established: ")
-            if not (self.validateHost(host) and self.validatePort(port)):
-                host = DEFAULT_HOST
-                port = DEFAULT_PORT
+            self.start_socket_server()
 
-        except ValueError:
-            print(f"Exception occured : {ValueError}")
+        except Exception as e:
+            print(f"Exception occured : {e}")
 
-    # Not required but keeping to learn as when connecting to socket it will throw exception if there is issue   
-    def validateHost(ip_address):
+    def start_socket_server(self):
+        """Starting a socket server with the defined configuration"""
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.host, self.port))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print(f"Connected by {addr}")
+                while True:
+                    data = conn.recv(1024)
+                    if not data:
+                        break
+                    conn.sendall(data)
+
+# Not required but keeping to learn as when connecting to socket it will throw exception if there is issue   
+    def __validateHost(self, ip_address):
         """Method to validate the ip address is a valid host or not."""
 
+        if ip_address == DEFAULT_HOST:
+            print("Using default host")
+            return False
+        
         match = re.match(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", ip_address)
 
         if not bool(match):
@@ -42,22 +69,26 @@ class SocketServer:
         print(f"IP address {ip_address} is valid")
         return True
 
-    def validatePort(port):
+    def __update_connection_ports(self, host, port):
+        if self.__using_default_ports(host, port):
+            return False
+        if (self.__validateHost(host) and self.__validatePort(port)):
+            return True
+        return False
+
+    def __using_default_ports(self, host, port):
+        if host == DEFAULT_HOST or port == DEFAULT_PORT:
+            print("Using default ports")
+            return True
+        return False
+
+    def __validatePort(self, port):
         """Method to validate if the port is a valid port that can be used."""
+
+        if port == DEFAULT_PORT:
+            print("Using default port")
+            return False
+        
         if port <= 1024 and port > 65535:
             return False
         return True
-
-    def start_socket_server():
-        """Starting a socket server with the defined configuration"""
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind((DEFAULT_HOST, DEFAULT_PORT))
-            s.listen()
-            conn, addr = s.accept()
-            with conn:
-                print(f"Connected by {addr}")
-                while True:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    conn.sendall(data)
